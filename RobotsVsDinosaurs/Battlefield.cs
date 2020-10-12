d4dd815c8c81bc4d30d86ab69b9484267837b14c
Forrest Morrisey <60801874+fmorrisey@users.noreply.github.com>
2020-10-12 16:18:33 -0500

    Menu class with list displays of robots or dinos. Bonus Menu decorators function for flair!

--- RobotsVsDinosaurs/Battlefield.cs
+++ RobotsVsDinosaurs/Battlefield.cs
@@ -13,6 +13,7 @@ namespace RobotsVsDinosaurs
         public Fleet fleetBravo; // Creates the Robot Fleet
         Dinosaur dinosaur; //Brings the Dino obj to the Battlefield
         Robot robot; //Brings the Robot obj to the battlefield
+        Menu Menu;
 
         Player player1;
         
@@ -22,6 +23,7 @@ namespace RobotsVsDinosaurs
             herdAlpha = new Herd();
             fleetBravo = new Fleet();
             player1 = new Player();
+            Menu = new Menu();
 
         }
 
@@ -29,58 +31,17 @@ namespace RobotsVsDinosaurs
         {
             //We will create some kind of loop and menu system
             //First Let's show our robots and dinos to the user
-            DisplayDinoHerd(herdAlpha.herdGroup);
-            DisplayRobotFleet(fleetBravo.fleetGroup);
+            Menu.DisplayDinoHerd(herdAlpha.herdGroup);
+            Menu.DisplayRobotFleet(fleetBravo.fleetGroup);
+            Menu.MenuDecorators("star");
+            
             Console.ReadLine();
         }
 
 
-        public void DisplayDinoHerd(List<Dinosaur> dinosaurs)
-        { // This creates and prints a list of Dinos for the player
-            Console.WriteLine("DAS DINO HERD!!! \n --------------------");
-                       
-            for (int i = 0; i < dinosaurs.Count; i++)
-            {
-                Console.Write($"| {dinosaurs[i].Type} |");
-                Console.Write($"{dinosaurs[i].Health} |");
-                Console.Write($"{dinosaurs[i].Energy} |");
-                Console.Write($"{dinosaurs[i].AttackType} |");
-                Console.WriteLine($"{dinosaurs[i].AttackPower} |");
-            }
-        }
+        
 
-
-        public void DisplayRobotFleet(List<Robot> robots)
-        { // This creates and prints a list of robots for the player
-            Console.WriteLine("DER ROBO Fleet!!! \n --------------------");
-
-            for (int i = 0; i < robots.Count; i++)
-            {
-                Console.Write($"| {robots[i].Name} |");
-                Console.Write($"{robots[i].Health} |");
-                Console.Write($"{robots[i].PowerLevel} |");
-                Console.Write($"{robots[i].Weapon} |");
-                Console.WriteLine($"{robots[i].AttackPower} |");
-            }
-        }
-
-        public void CWshrt()
-        {
-            void CWBlank()
-            {
-                Console.WriteLine();
-            }
-            
-            void CWDash()
-            {
-                Console.WriteLine("----------------");
-            }
-
-            void CWStar()
-            {
-                Console.WriteLine("****************");
-            }
-        }
+        
 
     }
 }
---
+++ RobotsVsDinosaurs/Menu.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaurs
+{
+    //This Class is focused on menus for the player to interact with
+    class Menu
+    {
+        public Herd herdAlpha; // Creates the Dino Herd
+        public Fleet fleetBravo; // Creates the Robot Fleet
+        Dinosaur dinosaur; //Brings the Dino obj to the Battlefield
+        Robot robot; //Brings the Robot obj to the battlefield
+
+        public Menu()
+        {
+            herdAlpha = new Herd();
+            fleetBravo = new Fleet();
+            
+
+        }
+
+        public void DisplayDinoHerd(List<Dinosaur> dinosaurs)
+        { // This creates and prints a list of Dinos for the player
+            Console.WriteLine("DAS DINO HERD!!!");
+            MenuDecorators("dash");
+
+            for (int i = 0; i < dinosaurs.Count; i++)
+            {
+                Console.Write($"| {dinosaurs[i].Type} |");
+                Console.Write($"{dinosaurs[i].Health} |");
+                Console.Write($"{dinosaurs[i].Energy} |");
+                Console.Write($"{dinosaurs[i].AttackType} |");
+                Console.WriteLine($"{dinosaurs[i].AttackPower} |");
+            }
+        }
+
+
+        public void DisplayRobotFleet(List<Robot> robots)
+        { // This creates and prints a list of robots for the player
+            Console.WriteLine("DER ROBO Fleet!!!");
+            MenuDecorators("dash");
+            
+            for (int i = 0; i < robots.Count; i++)
+            {
+                Console.Write($"| {robots[i].Name} |");
+                Console.Write($"{robots[i].Health} |");
+                Console.Write($"{robots[i].PowerLevel} |");
+                Console.Write($"{robots[i].Weapon} |");
+                Console.WriteLine($"{robots[i].AttackPower} |");
+            }
+        }
+
+
+        public void MenuDecorators(string Decoration)
+        { // call using the options to decorate the menues!
+            string parameterconvert = Decoration.ToLower();
+            switch (parameterconvert)
+            {
+                case "star": Console.WriteLine("***************");break;
+                case "dash": Console.WriteLine("---------------");break;
+                case "plus": Console.WriteLine("+++++++++++++++"); break;
+                case "equal": Console.WriteLine("==============="); break;
+                case "slashrt": Console.WriteLine("///////////////"); break;
+                case "slashlf": Console.WriteLine("\\\\\\\\\\\\\\\\"); break;
+                case "pipe": Console.WriteLine("|||||||||||||||||||"); break;
+                default: Console.WriteLine("/In/Valid//Menu//Decorator/"); break;
+            }
+            
+            
+        }
+    }
+}
