577b0d79e07a66d2c44311afc1f2bec9e02130d6
Forrest Morrisey <60801874+fmorrisey@users.noreply.github.com>
2020-10-13 17:00:06 -0500

    This version has current saves and meets MVP

--- RobotsVsDinosaurs/Program.cs
+++ RobotsVsDinosaurs/Program.cs
@@ -20,10 +20,9 @@ namespace RobotsVsDinosaurs
                 Battlefield battlefield = new Battlefield();
                 battlefield.RunGame();
                 newGame = menu.PlayAgain(playAgain);
-                Console.WriteLine("new game?" , newGame);
-                Console.WriteLine("play again?" , playAgain);
                 Thread.Sleep(1000);
             }
+            menu.Clear();
             Console.WriteLine("Thanks For Playing!!!");
             Console.WriteLine("Winners Don't Do Drugs");
             Console.WriteLine("FBI ANTI-PIRACY WARNING");
